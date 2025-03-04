312bc061cd69fce7bf0ea459f4cd5d19e5a62581
Artyom Gaibovich <artyomgyubov2002@gmail.com>
2025-03-04 15:20:47 +0300

    поправил файл

--- src/domain/entities/file.js
+++ src/domain/entities/file.js
@@ -1,15 +1,15 @@
 const {join} = require("node:path");
 
 class FileEntity {
-    constructor(browserId) {
-        this.browserId = browserId;
-        this.filePath = join(__dirname, '..', '..', '..', 'uploads', `${browserId}-browser_ids.json`);
+    constructor(browser_id) {
+        this.browser_id = browser_id;
+        this.browserIdFilePath = join(__dirname, '..', '..', '..', 'uploads', `${browser_id}-browser_ids.json`);
     }
 
     toJSON() {
         return {
-            browserId: this.browserId,
-            filePath: this.filePath,
+            browser_id: this.browser_id,
+            browserIdFilePath: this.browserIdFilePath,
         };
     }
 }
